6e78f7c95c32e625e5a10184807d19f6c3c16104
ThinkBIG-Company <info@think-big-company.com>
2020-07-30 09:57:23 +0200

    Version 4

--- src/localization.js
+++ src/localization.js
@@ -1,13 +1,13 @@
 export default {
   'langs': {
     'en-US': {
-      'helpers.localize_defaultlang': '[instantgram] set language: ${LANG_DEFAULT} \n For more information about available languages please check http://theus.github.io/instantgram',
+      'helpers.localize_defaultlang': '[instantgram] set language: ${LANG_DEFAULT} \n For more information about available languages please check http://thinkbig-company.github.io/instantgram',
 
-      'modules.update@oudated_outdated': '[instantgram] is outdated. Please check http://theus.github.io/instantgram for available updates.',
+      'modules.update@oudated_outdated': '[instantgram] is outdated. Please check http://thinkbig-company.github.io/instantgram for available updates.',
       'modules.update@oudated_localInfo': '[instantgram] Installed version: ${data.version} | New update: ${data.gitVersion}',
       'modules.update@determineIfGetUpdateIsNecessary_contacting': '[instantgram] is looking for available updates…',
       'modules.update@determineIfGetUpdateIsNecessary_updated': '[instantgram] updated your current version.',
-      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] found a new available update.\nPlease check <a href="http://theus.github.io/instantgram">http://theus.github.io/instantgram</a> to install it.',
+      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] found a new available update.\nPlease check <a href="http://thinkbig-company.github.io/instantgram">http://thinkbig-company.github.io/instantgram</a> to install it.',
 
       'index@alert_onlyWorks': '[instantgram] only works on instagram.com.',
       'index#program#modal@alert_dontFound': "[instantgram] didn't find any image in this Instagram post. Please try to open the link in a new tab.",
@@ -17,13 +17,13 @@ export default {
       'index#program@alert_videoBlob': "If you're trying open a video, try enter on Instagram post page. Like for example instagram.com/p/82jd828jd. If you're already in it, try refreshing the page before running [instantgram] or open the post in a new tab. (this proccess is neccessary only for videos)"
     },
     'pt-BR': {
-      'helpers.localize_defaultlang': '[instantgram] idioma configurado: ${LANG_DEFAULT} \npara mais informações sobre os idiomas suportados, acesse http://theus.github.io/instantgram',
+      'helpers.localize_defaultlang': '[instantgram] idioma configurado: ${LANG_DEFAULT} \npara mais informações sobre os idiomas suportados, acesse http://thinkbig-company.github.io/instantgram',
 
-      'modules.update@oudated_outdated': '[instantgram] está desatualizado. Acesse http://theus.github.io/instantgram para atualizar',
+      'modules.update@oudated_outdated': '[instantgram] está desatualizado. Acesse http://thinkbig-company.github.io/instantgram para atualizar',
       'modules.update@oudated_localInfo': '[instantgram] versão local: ${data.version} | nova versão: ${data.gitVersion}',
       'modules.update@determineIfGetUpdateIsNecessary_contacting': '[instantgram] está procurando atualizações...',
       'modules.update@determineIfGetUpdateIsNecessary_updated': '[instantgram] informações locais atualizadas',
-      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] encontrou uma atualização.\n acesse theus.github.io/instantgram para atualizar',
+      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] encontrou uma atualização.\n acesse thinkbig-company.github.io/instantgram para atualizar',
 
       'index@alert_onlyWorks': '[instantgram] somente funciona no instagram.com',
       'index#program#modal@alert_dontFound': '[instantgram] não encontrou uma imagem em um post. Tente abrir o link em uma nova aba.',
@@ -33,13 +33,13 @@ export default {
       'index#program@alert_videoBlob': "Se você está tentando abrir um vídeo, tente entrar na página do post do Instagram. ex: instagram.com/p/82jd828jd. Se você já está na página, tente atualizar a página antes de executar o [instantgram] ou abra o post em uma nova aba. (este processo só é necessário com vídeos)"
     },
     'de-DE': {
-      'helpers.localize_defaultlang': 'Ausgewählte Sprache: ${LANG_DEFAULT} \n Weitere Informationen zu den unterstützten Sprachen findest du auf http://theus.github.io/instantgram',
+      'helpers.localize_defaultlang': 'Ausgewählte Sprache: ${LANG_DEFAULT} \n Weitere Informationen zu den unterstützten Sprachen findest du auf http://thinkbig-company.github.io/instantgram',
 
-      'modules.update@oudated_outdated': '[instantgram] ist veraltet. Bitte besuche die Seite http://theus.github.io/instantgram für ein Update.',
+      'modules.update@oudated_outdated': '[instantgram] ist veraltet. Bitte besuche die Seite http://thinkbig-company.github.io/instantgram für ein Update.',
       'modules.update@oudated_localInfo': '[instantgram] Installierte Version ${data.version} | Neue Version: ${data.gitVersion}',
       'modules.update@determineIfGetUpdateIsNecessary_contacting': '[instantgram] sucht nach neuen verfügbaren Updates…',
       'modules.update@determineIfGetUpdateIsNecessary_updated': '[instantgram] wurde aktualisiert.',
-      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] hat ein neues Update gefunden.\nBitte besuche die Seite http://theus.github.io/instantgram, um das Update zu installieren.',
+      'modules.update@determineIfGetUpdateIsNecessary_@alert_found': '[instantgram] hat ein neues Update gefunden.\nBitte besuche die Seite http://thinkbig-company.github.io/instantgram, um das Update zu installieren.',
 
       'index@alert_onlyWorks': '[instantgram] funktioniert nur mit instagram.com.',
       'index#program#modal@alert_dontFound': "[instantgram] konnte kein Bild in diesem Post finden. Bitte öffne den Link in einem neuen Tab.",
