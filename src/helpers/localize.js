207ac91b256510cb68e5e247a74855ab82cb344e
ThinkBIG-Company <info@think-big-company.com>
2021-01-05 15:44:30 +0100

    v5.1.1
    Fix set language: undefined in dev console. Now the right language should displayed on github page

--- src/helpers/localize.js
+++ src/helpers/localize.js
@@ -1,13 +1,20 @@
 import localization from '../localization.js'
 
-const LANGS_NORMALIZE = {
-  'de': 'de-DE',
-  'pt': 'pt-BR',
-  'en': 'en-US',
-  'en-GB': 'en-US'
+let shortLang = navigator.language;
+if (shortLang.indexOf('-') !== -1) {
+    shortLang = shortLang.split('-')[0];
+}
+if (shortLang.indexOf('_') !== -1) {
+    shortLang = shortLang.split('_')[0];
 }
 
-const LANG_DEFAULT = LANGS_NORMALIZE[navigator.language]
+const LANGS_NORMALIZE = {
+    'de': 'de-DE',
+    'pt': 'pt-BR',
+    'en': 'en-US'
+}
+
+const LANG_DEFAULT = LANGS_NORMALIZE[shortLang]
 
 /**
  * @name: localize! function to return localized strings in localization.js
@@ -17,17 +24,19 @@ const LANG_DEFAULT = LANGS_NORMALIZE[navigator.language]
  *
  */
 
-function localize (str, lang = LANG_DEFAULT) {
-  try {
-    if (!localization.langs.hasOwnProperty(lang)) lang = 'en-US' // default lang
-    if (localization.langs[lang][str]) {
-      return localization.langs[lang][str]
-    }
-  } catch (e) {
-    console.error('[instantgram]LOC error:', e)
-    return `ops, an error ocurred in localization system. Enter in https://github.com/ThinkBIG-Company/instantgram/issues/new and open an issue with this code: 'LOC_dont_found_str_neither_default:[${lang}->${str}]'
+function localize(str, lang = LANG_DEFAULT) {
+    try {
+        if (!localization.langs.hasOwnProperty(lang)) {
+            lang = 'en-US' // default lang
+        }
+        if (localization.langs[lang][str]) {
+            return localization.langs[lang][str]
+        }
+    } catch (e) {
+        console.error('[instantgram]LOC error:', e)
+        return `ops, an error ocurred in localization system. Enter in https://github.com/ThinkBIG-Company/instantgram/issues/new and open an issue with this code: 'LOC_dont_found_str_neither_default:[${lang}->${str}]'
     for more information open the console`
-  }
+    }
 }
 
 console.info(localize('helpers.localize_defaultlang').replace('${LANG_DEFAULT}', LANG_DEFAULT))
